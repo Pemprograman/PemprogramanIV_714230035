65b84c27f1854b5799f3880b1b421514d7032fe1
akhrf <ahmadkarta9@gmail.com>
2025-12-10 17:39:37 +0700

    change

--- Pertemuan08/Praktikum/flutter_application_1/lib/main.dart
+++ Pertemuan08/Praktikum/flutter_application_1/lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
+import 'login_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.teal, useMaterial3: false),
+      home: const LoginScreen(),
     );
   }
 }
